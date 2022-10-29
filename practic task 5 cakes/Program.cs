8f54d884bbbabab5e04131d33c99b44eee5eef49
Giter228 <[200~isip_m.a.bordyan@mpt.ru>
2022-10-30 02:06:39 +0300

    upgraded

---
+++ practic task 5 cakes/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace practic_task_5_cakes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
